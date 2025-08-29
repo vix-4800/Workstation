1959364bc6ae56796694f4bb140f9d2d36864724
Anton Vix <Gordan.ei@protonmail.ch>
2025-08-30 03:29:09 +0400

    bootstraper init

---
+++ bootstraper/cmd/root.go
@@ -0,0 +1,28 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "bootstraper",
+	Short: "",
+	Long:  ``,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ bootstraper/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 Anton <Gordan.ei@protonmail.ch>
+*/
+package main
+
+import "github.com/bootstraper/bootstraper/cmd"
+
+func main() {
+	cmd.Execute()
+}
