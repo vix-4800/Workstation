c4a68c7ca83bcf9ed708d0bb31a543fd4b7811dc
Anton Vix <Gordan.ei@protonmail.ch>
2025-08-31 13:03:57 +0400

    link command update

--- bootstraper/cmd/link.go
+++ bootstraper/cmd/link.go
@@ -4,27 +4,38 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+type MapData map[string]map[string]string
+
 var linkCmd = &cobra.Command{
 	Use:   "link",
 	Short: "",
 	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-		jsonFile, err := os.Open("../map.json")
+		mapFile, err := os.ReadFile("../map.json")
 		if err != nil {
 			fmt.Printf("There was an error: %s", err)
 		}
 
-		var jsonData string
+		var mapData MapData
+		unmarhalErr := json.Unmarshal(mapFile, &mapData)
+		if unmarhalErr != nil {
+			fmt.Printf("There was an error parsing json data: %s", unmarhalErr)
+			panic(unmarhalErr)
+		}
 
-		jsonFile.Close()
-
-		fmt.Println(jsonData)
+		for category, categoryItems := range mapData {
+			for repoFilename, systemPath := range categoryItems {
+				fmt.Printf("%s/%s -> %s\n", category, repoFilename, systemPath)
+				// exec.Command("ln", "-sfn", repoFilename, systemPath)
+			}
+		}
 	},
 }
 
