ae9ba9c755fdc860dba5e61830fece19e508057a
Vix <Gordan.ei@protonmail.ch>
2025-09-06 21:57:31 +0400

    polybar bar renamed to main

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
 # Launch bar
-echo "---" | tee -a /tmp/polybar1.log
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
+echo "---" | tee -a /tmp/main.log
+polybar main 2>&1 | tee -a /tmp/main.log & disown
 
 echo "Bars launched..."
