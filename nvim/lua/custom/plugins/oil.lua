62cfa1a47f935d80aee4f97eacbfd9d2599170dd
Anton Vix <Gordan.ei@protonmail.ch>
2025-09-11 03:28:34 +0400

    oil installed to nvim

---
+++ nvim/lua/custom/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- icons
+}
