d715ebd5fb1d92bb0ee6116bcdbdf46ee3184019
Anton Vix <Gordan.ei@protonmail.ch>
2025-09-11 05:14:52 +0400

    new oli config

--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -1,10 +1,23 @@
 return {
   "stevearc/oil.nvim",
-  opts = {
-    default_file_explorer = true,
-    view_options = {
-      show_hidden = true,
-    },
-  },
+  opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- icons
+  config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+          natural_order = true,
+          is_always_hidden = function(name, _)
+            return name == '..' or name == '.git' or name == '.DS_Store' or name == 'thumbs.db'
+              or name == '.venv'
+          end,
+        },
+        win_options = {
+          wrap = true,
+        }
+      })
+    end,
 }
