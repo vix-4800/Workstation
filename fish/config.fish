020a17763e6c17d306d063d47e90698555c35d61
Anton Vix <Gordan.ei@protonmail.ch>
2025-09-13 20:08:48 +0400

    fan fish function added

--- fish/config.fish
+++ fish/config.fish
@@ -19,3 +19,12 @@ set -x PATH $PATH $HOME/.local/share/nvm/v24.7.0/bin /usr/local/go/bin $HOME/.co
 alias vim 'nvim'
 alias vi 'nvim'
 alias neovim 'nvim'
+
+function fan
+    if test (count $argv) -eq 0
+        echo "Usage: fan <speed %>"
+        return 1
+    end
+    sudo nvidia-settings -a "[gpu:0]/GPUFanControlState=1" -a "[fan:0]/GPUTargetFanSpeed=$argv[1]" --display :0
+    echo "Fan speed set to $argv[1]%"
+end
