bbde18b3534b546faac74a4488d88d15a516abad
Anton Vix <Gordan.ei@protonmail.ch>
2025-09-18 04:00:29 +0400

    fish readonly variables fix

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,10 @@ if test -f ~/.profile
     bash -c 'source ~/.profile; env' | while read line
         set item (string split -m 1 '=' $line)
         if test (count $item) -eq 2
-            set -gx $item[1] $item[2]
+            set var_name $item[1]
+            if not contains $var_name PWD SHLVL _ USER HOME SHELL
+                set -gx $var_name $item[2]
+            end
         end
     end
 end
