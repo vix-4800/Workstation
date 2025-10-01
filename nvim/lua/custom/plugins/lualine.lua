22d012db3e5c306b999377465e1edb224053d8ac
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-02 00:24:28 +0400

    lualine update

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -3,18 +3,28 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
+      icons_enabled = true,
       theme = 'auto',
+      component_separators = '',
+      section_separators = { left = '', right = '' },
       globalstatus = true,
-      disabled_filetypes = { statusline = { 'dashboard', 'alpha' } },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
     },
     sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'branch', 'diff'},
-      lualine_c = {'filename'},
-      lualine_x = {'filetype'},
-      lualine_y = {'progress'},
-      lualine_z = {'location'}
+      lualine_a = { 'mode' },
+      lualine_b = { 'filename', 'branch' },
+      lualine_c = { '%=' },
+      lualine_x = { 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
     },
+    tabline = {},
+    extensions = { 'oil' },
   },
   config = function(_, opts)
     require('lualine').setup(opts)
