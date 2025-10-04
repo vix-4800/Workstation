a1842251e675fc992de4668c39ad06ccb2e881f5
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-05 01:31:16 +0400

    alpha greeter added

---
+++ nvim/lua/custom/plugins/alpha-nvim.lua
@@ -0,0 +1,35 @@
+return {
+  'goolord/alpha-nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- dependencies = { 'echasnovski/mini.icons' },
+  config = function()
+    local dashboard = require 'alpha.themes.dashboard'
+    dashboard.section.header.val = {
+      '██╗   ██╗██╗██╗  ██╗',
+      '██║   ██║██║╚██╗██╔╝',
+      '██║   ██║██║ ╚███╔╝ ',
+      '╚██╗ ██╔╝██║ ██╔██╗ ',
+      ' ╚████╔╝ ██║██╔╝ ██╗',
+      '  ╚═══╝  ╚═╝╚═╝  ╚═╝',
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button('f', '  Find file', ':Telescope find_files <CR>'),
+      dashboard.button('r', '  Recent files', ':Telescope oldfiles <CR>'),
+      dashboard.button('p', '  Projects', ':Telescope projects <CR>'),
+      dashboard.button('s', '勒 Settings', ':e $MYVIMRC <CR>'),
+      dashboard.button('l', '鈴 Lazy', ':Lazy<CR>'),
+      dashboard.button('q', '  Quit', ':qa<CR>'),
+    }
+
+    dashboard.section.footer.val = ''
+
+    dashboard.section.footer.opts.hl = 'Type'
+    dashboard.section.header.opts.hl = 'Include'
+    dashboard.section.buttons.opts.hl = 'Keyword'
+
+    dashboard.opts.opts.noautocmd = true
+
+    require('alpha').setup(dashboard.opts)
+  end,
+}
