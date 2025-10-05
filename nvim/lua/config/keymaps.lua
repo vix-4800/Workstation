0619132ceb6fe6a027f64f6517416a1074ea8278
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-05 15:38:58 +0400

    telescope keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,14 @@
 local telescope = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Telescope [F]ind [F]iles" })
+vim.keymap.set("n", "<leader>sf", telescope.find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>sw", telescope.grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>sg", telescope.live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader><leader>", telescope.buffers, { desc = "[ ] Find existing buffers" })
+vim.keymap.set("n", "<leader>sb", function()
+  telescope.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+    winblend = 10,
+    previewer = false,
+  }))
+end, { desc = "Fuzzily search in current [b]uffer" })
 
 vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Open [E]xplorer" })
