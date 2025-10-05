4d2513b4d343b9e5123265f55e7f6054ee3844ae
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-06 03:29:57 +0400

    treesitter config update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,15 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  main = "nvim-treesitter.configs", -- Sets main module to use for opts
   branch = "master",
   lazy = false,
   build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "bash", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "vim", "vimdoc" },
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+    indent = { enable = true },
+  },
 }
