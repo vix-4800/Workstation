709bda4b99f2404ffc98c923935e345a9e2e5b9a
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-05 14:53:12 +0400

    options update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,9 +1,72 @@
-local global = vim.o
+local global = vim.opt
 
+-- Globals
 global.number = true
+global.relativenumber = true
+
 global.showmode = false
 
+global.termguicolors = true
+
+-- Better indentation
+global.breakindent = true
+global.smartindent = true -- Smart indenting
+global.autoindent = true -- Copy indent from current line when starting a new line
+
+-- Save undo history
+global.undofile = true
+
+-- Better search experience
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+global.ignorecase = true
+global.smartcase = true
+global.hlsearch = true -- Highlight search results
+global.incsearch = true -- Incremental search
+
+-- Keep signcolumn on by default
+global.signcolumn = 'yes'
+
+-- Decrease update time
+global.updatetime = 250
+
+-- Decrease mapped sequence wait time
+global.timeoutlen = 300
+
+-- Configure how new splits should be opened
+global.splitright = true
+global.splitbelow = true
+
+-- Preview substitutions live, as you type!
+global.inccommand = 'split'
+
+-- Show which line your cursor is on
+global.cursorline = true
+
+global.confirm = true
+
+-- Additional useful options
+global.fileencoding = 'utf-8' -- File content encoding
+
+-- Better completion experience
+global.completeopt = 'menuone,noselect' -- Better autocompletion
+global.pumheight = 10 -- Pop up menu height
+
+-- Better editing experience
+global.wrap = false -- Display lines as one long line
+global.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+global.sidescrolloff = 8 -- Minimal number of columns to keep to the left and right of the cursor
+
+global.backup = false -- Don't create backup files
+global.writebackup = false -- Don't create backup files
+global.swapfile = false -- Don't use swapfiles
+
+global.spell = true -- Enable spell checking
+global.spelllang = 'en' -- Set spellcheck language to English
+
+-- Other
 vim.g.mapleader = " "
+vim.g.maplocalleader = ' '
+
 vim.g.have_nerd_font = true
 
 vim.schedule(function()
