c9752150bc675240b9adef26b5971c5c19f1e40f
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-05 15:13:28 +0400

    explorer keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 local telescope = require("telescope.builtin")
 
-vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope [F]ind [F]iles' })
+vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Telescope [F]ind [F]iles" })
+
+vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Open [E]xplorer" })
