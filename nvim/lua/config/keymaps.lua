d3b3c2c1cc207b6c95f2d10a6a4ad746d2cff38e
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-05 23:33:03 +0400

    gitsigns added

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,4 @@
+-- ====== Telescope ======
 local telescope = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>sf", telescope.find_files, { desc = "[S]earch [F]iles" })
@@ -11,4 +12,27 @@ vim.keymap.set("n", "<leader>sb", function()
   }))
 end, { desc = "Fuzzily search in current [b]uffer" })
 
+-- ====== Oil ======
 vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", { desc = "Open [E]xplorer" })
+
+-- ====== GitSigns ======
+local gitsigns = require("gitsigns")
+
+-- visual mode
+vim.keymap.set("v", "<leader>gs", function()
+  gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+end, { desc = "git [s]tage hunk" })
+vim.keymap.set("v", "<leader>gr", function()
+  gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+end, { desc = "git [r]eset hunk" })
+
+-- normal mode
+vim.keymap.set("n", "<leader>gs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" })
+vim.keymap.set("n", "<leader>gS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
+vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" })
+vim.keymap.set("n", "<leader>gR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" })
+vim.keymap.set("n", "<leader>gu", gitsigns.stage_hunk, { desc = "git [u]ndo stage hunk" })
+vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk, { desc = "git [p]review hunk" })
+vim.keymap.set("n", "<leader>gd", function()
+  gitsigns.diffthis("@")
+end, { desc = "git [d]iff" })
