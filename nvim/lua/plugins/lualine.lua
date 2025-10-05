7845368f44b9a5f9829241eb877d02483da7e9b2
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-05 13:45:54 +0400

    lualine installed

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,32 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'auto',
+      component_separators = '',
+      section_separators = { left = '', right = '' },
+      globalstatus = true,
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'filename' },
+      lualine_c = { '%{FugitiveStatusline()}' },
+      lualine_x = { 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = { 'oil' },
+  },
+  config = function(_, opts)
+    require('lualine').setup(opts)
+  end,
+}
