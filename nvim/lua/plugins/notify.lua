d3ebc680f9b254014adb94808ba80f957b044688
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-05 23:40:22 +0400

    notify added

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -1,23 +1,23 @@
 -- Better notifications
 return {
-  'rcarriga/nvim-notify',
-  event = 'VeryLazy',
+  "rcarriga/nvim-notify",
+  event = "VeryLazy",
   config = function()
-    local notify = require('notify')
+    local notify = require("notify")
     notify.setup({
-      background_colour = 'NotifyBackground',
+      background_colour = "NotifyBackground",
       fps = 30,
       icons = {
-        DEBUG = '',
-        ERROR = '',
-        INFO = '',
-        TRACE = '✎',
-        WARN = '',
+        DEBUG = "",
+        ERROR = "",
+        INFO = "",
+        TRACE = "✎",
+        WARN = "",
       },
       level = 2,
       minimum_width = 50,
-      render = 'compact',
-      stages = 'fade_in_slide_out',
+      render = "compact",
+      stages = "fade_in_slide_out",
       timeout = 3000,
       top_down = true,
     })
