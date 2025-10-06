dc3ed138f1112d2c3c501e9e34c6df2e13d10685
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-06 15:30:05 +0400

    blink installed for lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -11,4 +11,46 @@ return {
       automatic_installation = false,
     },
   },
+  {
+    "saghen/blink.cmp",
+    event = "VimEnter",
+    dependencies = {
+      { "neovim/nvim-lspconfig" },
+      -- {'rafamadriz/friendly-snippets'}
+    },
+    version = "1.*",
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = "default",
+
+        -- Additional keymaps (optional)
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+      },
+
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
+      },
+
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      fuzzy = { implementation = "lua" },
+
+      signature = { enabled = true },
+    },
+    opts_extend = { "sources.default" },
+  },
 }
