7d48fdd16ec405ff7bae3aa062234d2f5fb92677
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-06 15:00:38 +0400

    formatting

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,17 +1,15 @@
 return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    -- { "j-hui/fidget.nvim", opts = {} },
+    -- {"saghen/blink.cmp"},
+  },
   {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- { "j-hui/fidget.nvim", opts = {} },
-      -- {"saghen/blink.cmp"},
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
+    opts = {
+      ensure_installed = {},
+      automatic_installation = false,
     },
-    {
-      "mason-org/mason-lspconfig.nvim",
-      dependencies = { "neovim/nvim-lspconfig" },
-      opts = {
-        ensure_installed = {},
-        automatic_installation = false,
-      }
-    },
-  }
+  },
 }
