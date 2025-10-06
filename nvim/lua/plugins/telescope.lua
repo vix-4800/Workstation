daac59a2445ff3f5b522fe6b5e76f3a5c204b210
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-06 12:58:43 +0400

    telescope config update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,5 +2,38 @@ return {
   "nvim-telescope/telescope.nvim",
   -- tag = '0.1.0',
   branch = "master",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  event = "VimEnter",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      cond = function()
+        return vim.fn.executable("make") == 1
+      end,
+    },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+    { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+  },
+  config = function()
+    require("telescope").setup({
+      -- defaults = {
+      --   mappings = {
+      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+      --   },
+      -- },
+      -- pickers = {}
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
+        },
+      },
+    })
+
+    local telescope = require("telescope")
+
+    -- Enable Telescope extensions if they are installed
+    pcall(telescope.load_extension, "fzf")
+    pcall(telescope.load_extension, "ui-select")
+  end,
 }
