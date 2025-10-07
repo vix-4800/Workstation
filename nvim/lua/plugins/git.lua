019a6d949f1e7128ef2f5aa21e22a618a027d17d
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-07 23:52:14 +0400

    refactor: consolidate git-related plugins into a single configuration file

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,88 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+      },
+      signs_staged = {
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+      },
+      signs_staged_enable = true,
+      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+      numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      watch_gitdir = {
+        follow_files = true,
+      },
+      auto_attach = true,
+      attach_to_untracked = false,
+      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+      sign_priority = 6,
+      update_debounce = 100,
+      status_formatter = nil,
+      max_file_length = 40000, -- Disable if file is longer than this (in lines)
+      preview_config = {
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1,
+      },
+    },
+    event = "BufReadPre",
+    config = function()
+      local gitsigns = require("gitsigns")
+
+      -- visual mode
+      vim.keymap.set("v", "<leader>gs", function()
+        gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, { desc = "git [s]tage hunk" })
+      vim.keymap.set("v", "<leader>gr", function()
+        gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, { desc = "git [r]eset hunk" })
+
+      -- normal mode
+      vim.keymap.set("n", "<leader>gs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" })
+      vim.keymap.set("n", "<leader>gS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
+      vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" })
+      vim.keymap.set("n", "<leader>gR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" })
+      vim.keymap.set("n", "<leader>gu", gitsigns.stage_hunk, { desc = "git [u]ndo stage hunk" })
+      vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk, { desc = "git [p]review hunk" })
+    end,
+  },
+  {
+    "tpope/vim-fugitive",
+  },
+  {
+    "sindrets/diffview.nvim",
+    event = "BufRead",
+    config = function()
+      local diffview = require("diffview")
+
+      vim.keymap.set("n", "<leader>gd", diffview.open, { desc = "Open Diffview" })
+      vim.keymap.set("n", "<leader>gq", diffview.close, { desc = "[Q]uit Diffview" })
+      vim.keymap.set("n", "<leader>gh", ":DiffviewFileHistory %<CR>", { desc = "File [H]istory" })
+      vim.keymap.set("n", "<leader>gH", ":DiffviewFileHistory<CR>", { desc = "Project [H]istory" })
+    end,
+  },
+}
