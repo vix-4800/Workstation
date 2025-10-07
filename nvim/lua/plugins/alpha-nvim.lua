ad98f61a4546c6bbf732a0fd3d2b7460877e9e35
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-07 17:29:08 +0400

    new dashboard option - new file

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -15,6 +15,7 @@ return {
 
     dashboard.section.buttons.val = {
       dashboard.button("f", "🔍 Find file", ":Telescope find_files <CR>"),
+      dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("r", "📄 Recent files", ":Telescope oldfiles <CR>"),
       dashboard.button("s", "⚙️ Settings", ":e $MYVIMRC <CR>"),
       dashboard.button("l", "  Lazy", ":Lazy<CR>"),
