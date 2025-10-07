659f83bdc43374ad12c0f7dcf986540c5d528f0b
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-07 20:09:02 +0400

    options refactoring

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -86,22 +86,24 @@ vim.diagnostic.config({
   severity_sort = true,
   float = { border = "rounded", source = "if_many" },
   underline = { severity = vim.diagnostic.severity.ERROR },
-  signs = vim.g.have_nerd_font and {
-    text = {
-      [vim.diagnostic.severity.ERROR] = "✗",
-      [vim.diagnostic.severity.WARN] = "⚠",
-      [vim.diagnostic.severity.INFO] = "ℹ",
-      [vim.diagnostic.severity.HINT] = "➤",
-    },
-    -- Highlight sign for selected severities
-    -- linehl = {
-    --   [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-    -- },
-    -- Highlight number for selected severities
-    numhl = {
-      [vim.diagnostic.severity.WARN] = "WarningMsg",
-    },
-  } or {},
+  signs = vim.g.have_nerd_font
+      and {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "✗",
+          [vim.diagnostic.severity.WARN] = "⚠",
+          [vim.diagnostic.severity.INFO] = "ℹ",
+          [vim.diagnostic.severity.HINT] = "➤",
+        },
+        -- Highlight sign for selected severities
+        -- linehl = {
+        --   [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+        -- },
+        -- Highlight number for selected severities
+        numhl = {
+          [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+      }
+    or {},
   virtual_text = {
     source = "if_many",
     spacing = 4,
