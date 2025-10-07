45a77ae8bc6da3a86ab1e44974834730c3c59d79
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-08 01:00:28 +0400

    blink signatures update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    -- {"saghen/blink.cmp"},
+    { "saghen/blink.cmp" },
   },
   {
     "mason-org/mason-lspconfig.nvim",
@@ -37,19 +37,31 @@ return {
       },
 
       completion = {
+        ghost_text = {
+          enabled = false,
+        },
         documentation = {
           auto_show = true,
-          auto_show_delay_ms = 500,
+        },
+        menu = {
+          draw = {
+            columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+            treesitter = { "lsp" },
+          },
         },
       },
-
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
       },
 
       fuzzy = { implementation = "lua" },
 
-      signature = { enabled = true },
+      signature = {
+        enabled = true,
+        window = {
+          show_documentation = true,
+        },
+      },
     },
     opts_extend = { "sources.default" },
   },
