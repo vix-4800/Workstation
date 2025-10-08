fad860313b0ebf1e348d6fef0f731300993fb4dd
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-08 23:10:30 +0400

    trim trailing whitespace autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,3 +5,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank()
   end,
 })
+
+-- Trim trailing whitespace on save, but preserve cursor position
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
