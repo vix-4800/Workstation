d3c31543c955fcdcc306bb317e3020363eca994c
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-08 23:31:54 +0400

    treesitter-textobjects added

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,9 @@ return {
   branch = "master",
   lazy = false,
   build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   opts = {
     ensure_installed = {
       "bash",
@@ -39,6 +42,54 @@ return {
         node_incremental = "<CR>",
         scope_incremental = "<TAB>",
         node_decremental = "<S-TAB>",
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ["af"] = { query = "@function.outer", desc = "around function" },
+          ["if"] = { query = "@function.inner", desc = "inner function" },
+          ["ac"] = { query = "@class.outer", desc = "around class" },
+          ["ic"] = { query = "@class.inner", desc = "inner class" },
+          ["aa"] = { query = "@parameter.outer", desc = "around parameter" },
+          ["ia"] = { query = "@parameter.inner", desc = "inner parameter" },
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ["]f"] = "@function.outer",
+          ["]c"] = "@class.outer",
+          ["]a"] = "@parameter.inner",
+        },
+        goto_next_end = {
+          ["]F"] = "@function.outer",
+          ["]C"] = "@class.outer",
+          ["]A"] = "@parameter.inner",
+        },
+        goto_previous_start = {
+          ["[f"] = "@function.outer",
+          ["[c"] = "@class.outer",
+          ["[a"] = "@parameter.inner",
+        },
+        goto_previous_end = {
+          ["[F"] = "@function.outer",
+          ["[C"] = "@class.outer",
+          ["[A"] = "@parameter.inner",
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>na"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>pa"] = "@parameter.inner",
+        },
       },
     },
   },
