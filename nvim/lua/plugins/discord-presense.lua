d7d2c8271972dbaf7c36017d4d3f3463c5fe4856
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-09 10:20:05 +0400

    discord-presense disabled

--- nvim/lua/plugins/discord-presense.lua
+++ nvim/lua/plugins/discord-presense.lua
@@ -1,35 +1,34 @@
 return {
-  'vyfor/cord.nvim',
-  build = ':Cord update',
+  "vyfor/cord.nvim",
+  build = ":Cord update",
   opts = function()
     local icons = require("config.icons")
     return {
       display = {
-        theme = 'catppuccin',
-        flavor = 'dark',
+        theme = "catppuccin",
+        flavor = "dark",
       },
       text = {
-        editing = icons.notifications.editing .. ' Editing files',
-        viewing = icons.notifications.viewing .. ' Viewing files',
+        editing = icons.notifications.editing .. " Editing files",
+        viewing = icons.notifications.viewing .. " Viewing files",
         workspace = function()
-          local hour = tonumber(os.date('%H'))
-          local status =
-            hour >= 23 and icons.notifications.midnight .. ' Midnight debugging session' or
-            hour >= 20 and icons.notifications.late_night .. ' Late-night refactoring' or
-            hour >= 17 and icons.notifications.evening .. ' Evening commits incoming' or
-            hour >= 13 and icons.notifications.afternoon .. ' Afternoon feature grind' or
-            hour >= 9  and icons.notifications.morning .. ' Morning coding flow' or
-            hour >= 6  and icons.notifications.early_bird .. ' Early-bird programming' or
-              icons.notifications.midnight .. ' After-hours hacking spree'
+          local hour = tonumber(os.date("%H"))
+          local status = hour >= 23 and icons.notifications.midnight .. " Midnight debugging session"
+            or hour >= 20 and icons.notifications.late_night .. " Late-night refactoring"
+            or hour >= 17 and icons.notifications.evening .. " Evening commits incoming"
+            or hour >= 13 and icons.notifications.afternoon .. " Afternoon feature grind"
+            or hour >= 9 and icons.notifications.morning .. " Morning coding flow"
+            or hour >= 6 and icons.notifications.early_bird .. " Early-bird programming"
+            or icons.notifications.midnight .. " After-hours hacking spree"
 
-          return string.format('%s', status)
-        end
-      }
+          return string.format("%s", status)
+        end,
+      },
     }
   end,
-  event = 'VeryLazy',
-  enabled = true,
+  event = "VeryLazy",
+  enabled = false,
   keys = {
-    { '<leader>td', ':Cord toggle<CR>', desc = '[T]oggle [D]iscord Presence' },
+    { "<leader>td", ":Cord toggle<CR>", desc = "[T]oggle [D]iscord Presence" },
   },
 }
