66fab0c717c527a49b69b73c2d8403ad541155b9
Anton Vix <Gordan.ei@protonmail.ch>
2025-10-28 13:06:07 +0400

    Add is_laptop function and update environment configuration

--- config/fish/conf.d/00-environment.fish
+++ config/fish/conf.d/00-environment.fish
@@ -5,3 +5,11 @@ set -x VISUAL $EDITOR
 set -gx TERM xterm-256color
 
 set -x NODE_VERSION v24.7.0
+
+if not set -q SYSTEM_TYPE
+    if is_laptop
+        set -gx SYSTEM_TYPE laptop
+    else
+        set -gx SYSTEM_TYPE desktop
+    end
+end
