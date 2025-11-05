ad0920b6c527e6c30bad7a6e8f168ac737d5307d
Anton Vix <Gordan.ei@protonmail.ch>
2025-11-06 00:45:07 +0400

    feat: add custom kind icons for improved LSP completion appearance

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -386,6 +386,34 @@ return {
 
       appearance = {
         nerd_font_variant = "mono",
+
+        kind_icons = {
+          Text = " ",
+          Method = " ",
+          Function = " ",
+          Constructor = " ",
+          Field = " ",
+          Variable = " ",
+          Class = " ",
+          Interface = " ",
+          Module = " ",
+          Property = " ",
+          Unit = " ",
+          Value = " ",
+          Enum = " ",
+          Keyword = " ",
+          Snippet = " ",
+          Color = " ",
+          File = " ",
+          Reference = " ",
+          Folder = " ",
+          EnumMember = " ",
+          Constant = " ",
+          Struct = " ",
+          Event = " ",
+          Operator = " ",
+          TypeParameter = " ",
+        },
       },
 
       completion = {
