9d5e81cbbeecee9ccd3d945617f633ddc40c5577
Anton Vix <Gordan.ei@protonmail.ch>
2026-03-12 17:40:04 +0400

    feat: enhance refactor function to support multiple files and improve usage instructions

--- config/fish/functions/refactor.fish
+++ config/fish/functions/refactor.fish
@@ -1,20 +1,20 @@
-function refactor --description "Run rector and php-cs-fixer on a file or directory"
+function refactor --description "Run rector and php-cs-fixer on one or more files or directories"
     if test (count $argv) -eq 0
-        echo "Usage: refactor <file|directory>"
+        echo "Usage: refactor <file|directory> [file|directory ...]"
         return 1
     end
 
-    set target $argv[1]
-
-    if not test -e "$target"
-        echo "Error: '$target' does not exist"
-        return 1
+    for target in $argv
+        if not test -e "$target"
+            echo "Error: '$target' does not exist"
+            return 1
+        end
     end
 
-    set rector_out (rector process "$target" --config "$HOME/.config/rector/rector.php" --no-ansi 2>&1)
+    set rector_out (rector process $argv --config "$HOME/.config/rector/rector.php" --no-ansi 2>&1)
     set rector_summary (printf '%s\n' $rector_out | grep '\[OK\]' | string trim | string replace '[OK] ' '')
 
-    set fixer_out (php-cs-fixer fix "$target" --config "$HOME/.config/php-cs-fixer/php-cs-fixer.php" --no-ansi 2>&1)
+    set fixer_out (php-cs-fixer fix $argv --config "$HOME/.config/php-cs-fixer/php-cs-fixer.php" --no-ansi 2>&1)
     set fixer_summary (printf '%s\n' $fixer_out | grep '^Fixed' | string replace -r ' in .*' '')
 
     echo "Rector:       "(test -n "$rector_summary" && echo $rector_summary || echo "no changes")
