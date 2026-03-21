530a9e7d63988921d760a3bf554364acdb5fb9ac
Anton Vix <Gordan.ei@protonmail.ch>
2026-03-22 01:34:04 +0400

    chore(editor): skip trailing whitespace trim for markdown
    Exclude markdown files from the BufWritePre autocmd that strips trailing whitespace, since markdown uses two trailing spaces for hard line breaks.

--- roles/editor/files/nvim/lua/config/autocmds.lua
+++ roles/editor/files/nvim/lua/config/autocmds.lua
@@ -7,9 +7,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Trim trailing whitespace on save, but preserve cursor position
+-- Skip markdown where trailing spaces are significant (hard line break)
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   callback = function()
+    if vim.bo.filetype == "markdown" then
+      return
+    end
     local save_cursor = vim.fn.getpos(".")
     vim.cmd([[%s/\s\+$//e]])
     vim.fn.setpos(".", save_cursor)
